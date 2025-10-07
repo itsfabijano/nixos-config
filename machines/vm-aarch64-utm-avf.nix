5ae6c4f4b24e5ef3a7b2ef8c6c9c33b23b453a23
itsfabijano <fabian.ehringer@gmx.net>
2025-10-07 16:20:05 +0000

    update

--- machines/vm-aarch64-utm-avf.nix
+++ machines/vm-aarch64-utm-avf.nix
@@ -12,5 +12,22 @@
     #     device = "/dev/disk/by-label/nixos";
     #     fsType = "ext4";
     # };
+
+    # load the driver
+    boot.kernelModules = [ "virtiofs" ];
+
+    # declarative mount
+    fileSystems."/mnt/utm" = {
+        device = "share";    # must match the tag name you gave in UTM’s “Shared Directory”
+        fsType = "virtiofs";
+        options = [
+            "rw"
+            "nofail"
+            "_netdev"
+            "noauto"
+            "x-systemd.automount"  # optional: mount on first access
+            # "x-systemd.idle-timeout=300"  # optional: unmount after 5 min idle
+        ];
+    };
 }
 
