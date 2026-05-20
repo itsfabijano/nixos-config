ac0669da1242a6b098031383310ee9329b668ed6
itsfabijano <fabian.ehringer@gmx.net>
2026-05-20 16:32:39 +0000

    fix devshells generate function

--- lib/mk-devshells.nix
+++ lib/mk-devshells.nix
@@ -2,8 +2,7 @@
 { system }:
 
 let
-    systemFunc = nixpkgs.lib.nixosSystem;
     pkgs = import nixpkgs { inherit system; config.allowUnfree = true; inherit overlays; };
-in devshellFunc {
-    dotnet8 = import ./devshells/dotnet8/shell.nix { inherit pkgs; };
+in {
+    dotnet8 = import ../devshells/dotnet8/shell.nix { inherit pkgs; };
 }
